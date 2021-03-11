dc12e5580220a600d58bda3e3e57f5cc00d80a3a
AngelinaZh <angelinka1921@gmail.com>
2021-03-11 14:15:49 +0300

    feat:update with additing information

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array)  {
+  if (typeof array == "undefined"){
+    return 0;
+  } else if (array.length == 0){
+    return 0;
+  }
+  else {
+    return Math.min.apply(null, array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array == "undefined"){
+    return 0;
+  } else if (array.length == 0){
+    return 0;
+  }
+  else {
+    return Math.max.apply(null, array);
+  } 
 }
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg (array){
+  let sum=0;
+   for (let i in array) {
+     sum += array[i];
+   }
+   let count = array.length;
+   if (typeof array == "undefined"){
+     return 0;
+   } else if (array.length == 0){
+     return 0;
+   }else{
+     return sum /count;
+   }
+ }
